b14d6bbf99f282cdacb356b55fbd1479be810541
1131-sweb-demo21341032 <3398621320@qq.com>
2025-03-16 21:20:31 +0800

    W04-P3: Show sdata statistics from js code

--- demo/w04_basics_26/p2_26/p2_1_26.js
+++ demo/w04_basics_26/p2_26/p2_1_26.js
@@ -1,21 +1,59 @@
-import { sdata } from './data_26.js';
-console.log('sdata', sdata);
+import { sdata2 } from './data_26.js';
+console.log('sdata2', sdata2);
 
 const result1 = document.querySelector('.result1');
 const result2 = document.querySelector('.result2');
+
+const computePass = (s) => {
+    let pass = 0;
+    s.forEach((score) => {
+        if (score >= 59.9) pass++;
+    });
+    return pass;
+};
+
+const computeFail = (s) => {
+    // Fails are those scores below 59.9.
+    return s.length - computePass(s);
+};
+
+const computeHighest = (s) => {
+    let highest = -100;
+    s.forEach((score) => {
+        if (score > highest) highest = score;
+    });
+    return highest;
+};
+
+const computeLowest = (s) => {
+    let lowest = 200;
+    s.forEach((score) => {
+        if (score < lowest) lowest = score;
+    });
+    return lowest;
+};
+
+const computeAverage = (s) => {
+    let sum = 0;
+    s.forEach((score) => {
+        sum += score;
+    });
+    return sum / s.length;
+};
 
 const outputStat1 = (s) => {
     result1.innerHTML = `
         <h3 class="my-4">Array Statistics</h3>
         <p>sdata original:</p>
         <p>${JSON.stringify(s)}</p>
-        <p>Total: 6</p>
-        <p>Pass: 4</p>
-        <p>Fail: 2</p>
-        <p>Highest: 100</p>
-        <p>Lowest: 20</p>
-        <p>Average: 65.2</p>
+        <p>Total: ${s.length}</p>
+        <p>Pass: ${computePass(s)}</p>
+        <p>Fail: ${computeFail(s)}</p>
+        <p>Highest: ${computeHighest(s)}</p>
+        <p>Lowest: ${computeLowest(s)}</p>
+        <p>Average: ${computeAverage(s).toFixed(1)}</p>
     `;
 };
 
-outputStat1(sdata);
+outputStat1(sdata2);
+
