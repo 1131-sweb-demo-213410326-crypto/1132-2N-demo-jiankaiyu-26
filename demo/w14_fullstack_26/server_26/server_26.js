a2a45e600a12d9793ddbcb3cc5b3a0cbd6f8e72c
1131-sweb-demo21341032 <3398621320@qq.com>
2025-05-31 21:46:43 +0800

    W14-P2: Show static products page

--- demo/w14_fullstack_26/server_26/server_26.js
+++ demo/w14_fullstack_26/server_26/server_26.js
@@ -5,6 +5,14 @@ const app_26 = express();
 app_26.use(express.static('public'));
 app_26.set('view engine', 'ejs');
 
+app_26.use('/product_26/static', (req, res, next)=>{
+    res.render('product_26/static_26',{
+    title:'Get Products - Static',
+    name:'jiankaiyu',
+    id:'213410326',
+    });
+});
+
 app_26.use('/',(req, res, next)=>{
 res.render('index',{
     title:'Express',
